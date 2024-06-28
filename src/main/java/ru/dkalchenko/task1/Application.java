ef92b80649a1dc7618932d2267561c178758b216
denfort50 <denfort50@yandex.ru>
2024-06-28 10:18:02 +0300

    Перекомпоновка реализованной работы

--- src/main/java/ru/dkalchenko/task1/Application.java
+++ src/main/java/ru/dkalchenko/task1/Application.java
@@ -1,15 +1,10 @@
-package ru.dkalchenko;
+package ru.dkalchenko.task1;
 
-import ru.dkalchenko.task1.TestRunnerTest;
 import ru.dkalchenko.task1.processor.TestRunner;
 
-public class App {
+public class Application {
 
     public static void main(String[] args) {
-        runFirstTask();
-    }
-
-    private static void runFirstTask() {
         System.out.println("Домашнее задание №1 выполнено.");
         System.out.println("====================РЕЗУЛЬТАТ====================");
         TestRunner.runTests(TestRunnerTest.class);
